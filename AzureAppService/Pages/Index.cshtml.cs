2ddd929caa6b79e556e42784a0314b6b2654858f
Adrian Cheong <adriancheong@gic.com.sg>
2018-03-06 16:39:31 +0800

    Added config injection

--- AzureAppService/Pages/Index.cshtml.cs
+++ AzureAppService/Pages/Index.cshtml.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AzureAppService.Pages
 {
     public class IndexModel : PageModel
     {
+        IConfiguration config;
+        public string Environment { get; set; }
+        public IndexModel(IConfiguration config)
+        {
+            Environment = config["ASPNETCORE_ENVIRONMENT"];
+        }
+
         public void OnGet()
         {
 
